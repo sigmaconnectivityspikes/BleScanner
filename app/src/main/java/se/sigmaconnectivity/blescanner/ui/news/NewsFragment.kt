472fd3a8dd7e22fc94fc6984ebac9e3bc7388cb7
Mateusz Wlazłowski <mateusz.wlazlowski@sigmaconnectivity.se>
2020-04-01 15:36:35 +0200

    Add webView on news screen contains MZ_GOV_PL twitter site

--- app/src/main/java/se/sigmaconnectivity/blescanner/ui/news/NewsFragment.kt
+++ app/src/main/java/se/sigmaconnectivity/blescanner/ui/news/NewsFragment.kt
@@ -1,23 +1,46 @@
 package se.sigmaconnectivity.blescanner.ui.news
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import se.sigmaconnectivity.blescanner.R
+import se.sigmaconnectivity.blescanner.databinding.FragmentNewsBinding
 
-/**
- * A simple [Fragment] subclass.
- */
 class NewsFragment : Fragment() {
+
+    private lateinit var binding: FragmentNewsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_news,
+            container,
+            false
+        )
+        binding.lifecycleOwner = this
+
+        initWebView()
+
+        return binding.root
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun initWebView() {
+        val url = getString(R.string.news_url)
+        binding.webView.apply {
+            settings.javaScriptEnabled = true
+            settings.domStorageEnabled = true
+            webViewClient = WebViewClient()
+            loadUrl(url)
+        }
     }
 
 }
