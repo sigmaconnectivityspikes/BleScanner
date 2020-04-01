5db0e728964897ed94448cb888807d58a85132c1
wojciech <wojciech.nasilowski@sigmaconnectivity.se>
2020-04-01 14:37:23 +0200

    Fix checksum generation

--- app/src/main/java/se/sigmaconnectivity/blescanner/SharedPrefs.kt
+++ app/src/main/java/se/sigmaconnectivity/blescanner/SharedPrefs.kt
@@ -2,9 +2,11 @@ package se.sigmaconnectivity.blescanner
 
 import android.content.Context
 import androidx.core.content.edit
-import se.sigmaconnectivity.blescanner.utils.Crc64Util
 import timber.log.Timber
+import java.math.BigInteger
+import java.security.MessageDigest
 import java.util.*
+
 
 class SharedPrefs(private val context: Context) {
     private val prefs by lazy {
@@ -19,12 +21,16 @@ class SharedPrefs(private val context: Context) {
         Timber.d("Generated UUID = $uuid")
     }
 
-    fun getUserId(): Long? = if(getUserUuid().isBlank()) null else getUserUuid().toCrc()
+    fun getUserId(): Long? = if(getUserUuid().isBlank()) null else getUserUuid().toHash()
 
     private fun getUserUuid(): String {
         val uuid = prefs.getString(Consts.SHARED_PREFS_UUID, "").toString()
         Timber.d("Current UUID = $uuid")
         return uuid
     }
-    private fun String.toCrc() = Crc64Util.checksum(toByteArray(Charsets.UTF_8))
+    private fun String.toHash(): Long {
+        val md = MessageDigest.getInstance("SHA-1")
+        md.update(toByteArray(Charsets.UTF_8))
+        return BigInteger(md.digest()).toLong()
+    }
 }
