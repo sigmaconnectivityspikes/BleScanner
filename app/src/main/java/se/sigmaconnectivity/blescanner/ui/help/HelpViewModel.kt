3b92f30b6b757fec5f93f1b20e6e3ccf49a9d61d
Mateusz Wlazłowski <mateusz.wlazlowski@sigmaconnectivity.se>
2020-04-02 16:13:23 +0200

    Change QR code background color to transparent

--- app/src/main/java/se/sigmaconnectivity/blescanner/ui/help/HelpViewModel.kt
+++ app/src/main/java/se/sigmaconnectivity/blescanner/ui/help/HelpViewModel.kt
@@ -1,14 +1,21 @@
 package se.sigmaconnectivity.blescanner.ui.help
 
+import android.graphics.Bitmap
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.LiveDataReactiveStreams
 import androidx.lifecycle.ViewModel
-import io.reactivex.disposables.CompositeDisposable
+import com.google.zxing.BarcodeFormat
+import com.journeyapps.barcodescanner.BarcodeEncoder
 import se.sigmaconnectivity.blescanner.domain.usecase.GetHumanReadableUserIdUseCase
 
-class HelpViewModel(private val getHumanReadableUserIdUseCase: GetHumanReadableUserIdUseCase) : ViewModel() {
-    private val compositeDisposable = CompositeDisposable()
+class HelpViewModel(private val getHumanReadableUserIdUseCase: GetHumanReadableUserIdUseCase) :
+    ViewModel() {
 
-    val userId: LiveData<String> =
-        LiveDataReactiveStreams.fromPublisher(getHumanReadableUserIdUseCase.execute().toFlowable())
+    val userId: LiveData<Bitmap> =
+        LiveDataReactiveStreams.fromPublisher(
+            getHumanReadableUserIdUseCase.execute()
+                .map { userId: String ->
+                    BarcodeEncoder().encodeBitmap(userId, BarcodeFormat.QR_CODE, 500, 500)
+                }.toFlowable()
+        )
 }
