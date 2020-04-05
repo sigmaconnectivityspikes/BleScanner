57a09920149176b91232bf39bf9e6865e1c7cb29
Pawel Kleczkowski <pawel.kleczkowski@sigmaconnectivity.se>
2020-04-05 13:17:05 +0200

    Dev merged.

--- data/src/main/java/se/sigmaconnectivity/blescanner/data/HashExtensions.kt
+++ data/src/main/java/se/sigmaconnectivity/blescanner/data/HashExtensions.kt
@@ -2,12 +2,6 @@ package se.sigmaconnectivity.blescanner.data
 
 import java.security.MessageDigest
 
-fun String.toHash(): ByteArray {
-    val md = MessageDigest.getInstance("SHA-1")
-    md.update(toByteArray(Charsets.UTF_8))
-    return md.digest().sliceArray(0 until HASH_SIZE_BYTES)
-}
-
 fun ByteArray.toChecksum(): Byte {
     var sum = 0
     forEach {
