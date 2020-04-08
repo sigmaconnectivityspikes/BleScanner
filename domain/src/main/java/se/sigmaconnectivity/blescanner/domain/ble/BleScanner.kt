d73a42c0cccfb9c6b5cd4788b0a3d02865b50ef3
wojciech <wojciech.nasilowski@sigmaconnectivity.se>
2020-04-08 14:00:52 +0200

    Two services filer

--- domain/src/main/java/se/sigmaconnectivity/blescanner/domain/ble/BleScanner.kt
+++ domain/src/main/java/se/sigmaconnectivity/blescanner/domain/ble/BleScanner.kt
@@ -5,10 +5,10 @@ import se.sigmaconnectivity.blescanner.domain.model.BLEFeatureState
 import se.sigmaconnectivity.blescanner.domain.model.ScanResultItem
 
 interface BleScanner {
-    fun scanBleDevicesWithTimeout(
-        serviceUuid: String,
-        timeoutMillis: Long
-    ): Observable<ScanResultItem>
 
     val trackScanningStatus: Observable<BLEFeatureState>
+    fun scanBleDevicesWithTimeout(
+        serviceUuids: List<String>,
+        timeoutMillis: Long
+    ): Observable<ScanResultItem>
 }
