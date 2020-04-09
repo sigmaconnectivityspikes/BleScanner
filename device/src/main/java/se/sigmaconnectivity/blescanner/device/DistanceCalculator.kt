de5eb9656d6f7a9d3c0fafd9881dbc0ba8c38c46
wojciech <wojciech.nasilowski@sigmaconnectivity.se>
2020-04-09 10:46:23 +0200

    Distance measuring first approach

--- device/src/main/java/se/sigmaconnectivity/blescanner/device/DistanceCalculator.kt
+++ device/src/main/java/se/sigmaconnectivity/blescanner/device/DistanceCalculator.kt
@@ -4,7 +4,6 @@ import kotlin.math.pow
 
 class DistanceCalculator {
     fun calculate(rssi: Int, txPower: Int): Double {
-        println("Contact items-  $rssi $txPower calc ${10.0.pow((txPower.toDouble() - rssi) / (10 * 2))}")
         return 10.0.pow((txPower.toDouble() - rssi) / (10 * 2))
     }
 }
