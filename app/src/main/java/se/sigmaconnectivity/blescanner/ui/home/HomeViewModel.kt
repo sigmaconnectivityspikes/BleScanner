bd00d9cb60d5bd4d9df9d9abdd63bd6204521e97
wojciech <wojciech.nasilowski@sigmaconnectivity.se>
2020-04-15 13:44:34 +0200

    usecases for bridging data

--- app/src/main/java/se/sigmaconnectivity/blescanner/ui/home/HomeViewModel.kt
+++ app/src/main/java/se/sigmaconnectivity/blescanner/ui/home/HomeViewModel.kt
@@ -1,18 +1,25 @@
 package se.sigmaconnectivity.blescanner.ui.home
 
+import se.sigmaconnectivity.blescanner.domain.model.IncomingBridgeDataItem
+import se.sigmaconnectivity.blescanner.domain.model.IncomingBridgeDataType
+import se.sigmaconnectivity.blescanner.domain.model.OutgoingBridgeDataType
+import se.sigmaconnectivity.blescanner.domain.usecase.OnGetBridgeDataUseCase
+import se.sigmaconnectivity.blescanner.domain.usecase.OnSetBridgeDataUseCase
 import se.sigmaconnectivity.blescanner.ui.common.BaseViewModel
 
-class HomeViewModel() : BaseViewModel() {
-
+class HomeViewModel(
+    private val onSetBridgeDataUseCase: OnSetBridgeDataUseCase,
+    private val onGetBridgeDataUseCase: OnGetBridgeDataUseCase
+    ) : BaseViewModel() {
 
     fun setBridgeData(dataType: Int, dataJson: String) {
-       //DUMMY
+        onSetBridgeDataUseCase.execute(IncomingBridgeDataItem(
+            type = IncomingBridgeDataType.valueOf(dataType),
+            payload = dataJson
+        ))
     }
 
     fun getBridgeData(dataType: Int): String {
-        return "DUMMY"/*Gson().toJson( //TODO gson model
-            contactUseCase.getContacts()
-                .blockingFirst()
-        )*/
+        return onGetBridgeDataUseCase.execute(OutgoingBridgeDataType.valueOf(dataType))
     }
 }
--- domain/src/main/java/se/sigmaconnectivity/blescanner/domain/model/IncomingBridgeDataType.kt
+++ domain/src/main/java/se/sigmaconnectivity/blescanner/domain/model/IncomingBridgeDataType.kt
@@ -1,3 +1,10 @@
 package se.sigmaconnectivity.blescanner.domain.model
 
-enum class IncomingBridgeDataType(val code: Int)
+enum class IncomingBridgeDataType(val code: Int) {
+    HASH_ID(0);
+
+
+    companion object {
+        fun valueOf(value: Int): IncomingBridgeDataType = IncomingBridgeDataType.values().find { it.code == value } ?: throw IllegalAccessException()
+    }
+}
