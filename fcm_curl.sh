e3f172a13b9675f982a99572abd15d6975dd2e10
wojciech <wojciech.nasilowski@sigmaconnectivity.se>
2020-04-15 09:15:38 +0200

    Distance measuring first approach

--- fcm_curl.sh
+++ fcm_curl.sh
@@ -1,5 +1,5 @@
 #command to send sample fcm information about infection
-curl -X POST --header "Authorization: key=AAAAuyaOjSg:APA91bHPVcFp7Wh2O_B4JMujo5aeNEoBjXaSWbkwag6Qf8Qgo-WHFYPwXjECiPmswgVoGR-CpsZPRZo7N_tBWBvj5UBevqhw94f4lNBkQg9a-5gam_kjhztv9XC0STNQ5x-B-mys3n0z"     --Header "Content-Type: application/json"     https://fcm.googleapis.com/fcm/send     -d '{"to":"/topics/infections",
+curl -X POST --header "Authorization: key=AAAAbK64j8I:APA91bHdFzSaBpAlzYDUvxQU_cgqFqd7U2lncyzCCZFs6_bKRcyt2OJuWn9RI1fA6S1KhbBfICof3AJLmfc2zv9HuOaRhZMYFNDtM9qyqHExcT-ahuAkMYEZSOVRh3OZJjDN-n4DXHdt"     --Header "Content-Type: application/json"     https://fcm.googleapis.com/fcm/send     -d '{"to":"/topics/infections",
 "notification":{
 "title":"New infection",
       "body":"New infection detected",
